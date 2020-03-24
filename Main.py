7b56b135ae514e0bd38e0a7fbd59d88ff5ccec92
Wojciech Legutowski <wlegutowski@gmail.com>
2020-03-24 14:05:37 +0100

    /Date => /Data

--- Main.py
+++ Main.py
@@ -4,6 +4,6 @@ import  numpy as np
 import pandas as pd
 
 
-df = pd.read_csv("Date/tracking_data.rpt", skiprows=[1], nrows=150)
+df = pd.read_csv("Data/tracking_data.rpt", skiprows=[1], nrows=150)
 
 print(df.head())

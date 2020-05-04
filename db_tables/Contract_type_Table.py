d6df5ae61d8a0935366b46ed9b03fb61182f5320
Patryk Abraś <34245127+patrykabras@users.noreply.github.com>
2020-05-04 14:16:46 +0200

    Created placeholder for table Class

---
+++ db_tables/Contract_type_Table.py
@@ -0,0 +1,13 @@
+import mysql.connector
+from mysql.connector import errorcode
+from data_db_connector.DBConnector import DBConnector
+
+
+class Contract_type_Table:
+
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
---
+++ db_tables/Postcode_Table.py
@@ -0,0 +1,14 @@
+import mysql.connector
+from mysql.connector import errorcode
+from data_db_connector.DBConnector import DBConnector
+
+
+class Postcode_Table:
+
+    def __init__(self):
+        pass
+
+    def __del__(self):
+        pass
+
+
